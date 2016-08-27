65b97c8a45b590be109de5653e5976aa1be52104
Tim Chen <tim.chen119@canonical.com>
2016-08-27 15:33:46 +0800

    updated with latest libefivar and libfwup

--- fwupd.sh
+++ fwupd.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 export XDG_DATA_DIRS="$SNAP/usr/share"
+if [ ! -f $SNAP_DATA/etc/fwupd.conf ]
+then
+        mkdir -p $SNAP_DATA/etc
+        cp $SNAP/etc/fwupd.conf $SNAP_DATA/etc/fwupd.conf
+fi
+if [ ! -d $SNAP_DATA/etc/pki ]
+then
+        cp -r $SNAP/etc/pki $SNAP_DATA/etc/
+fi
 exec "$SNAP/libexec/fwupd/fwupd" $@
