9a177f07e24440325ad4534a62836c35c42975c0
Tim Chen <tim.chen119@canonical.com>
2017-04-25 18:11:43 +0800

    Add http_proxy and https_proxy support

--- startup-wrapper/fwupdmgr.sh
+++ startup-wrapper/fwupdmgr.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+export XDG_DATA_DIRS="$SNAP/usr/share"
 export GIO_EXTRA_MODULES="$SNAP/usr/lib/x86_64-linux-gnu/gio/modules"
+
+#TODO: once polkit is integrated to snappy it should revisted, currently this works for root-only.
+XDG_RUNTIME_DIR="$SNAP_DATA/run/user/$(id -u)"
+if [ ! -d $XDG_RUNTIME_DIR ]
+then
+	mkdir -p $XDG_RUNTIME_DIR
+fi
 
 #A workaround for the fchown secomp denial that crash fwupd, used in fu_main_daemon_update_metadata called as_store_to_file and then gio.
 #audit: type=1326 audit(1472623873.021:86): auid=4294967295 uid=0 gid=0 ses=4294967295 pid=1468 comm="fwupd"
